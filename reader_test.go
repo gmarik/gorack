5453ced7d3627172f9e75ec93e52fdb392420153
gmarik <gmarik@gmail.com>
2014-12-14 16:51:09 -0500

    ok looks like it's working

--- reader_test.go
+++ reader_test.go
@@ -2,6 +2,7 @@ package gorack
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -25,51 +26,106 @@ hello world!
 `
 )
 
+const MAX_HEADERS_SIZE = 512 * 16 * 1024 // 256 * 16K
+
 type RackResponse struct {
 	rackResponse io.Reader
 	Headers      map[string][]string
 	Status       int
 	Body         io.Reader
+
+	buf         *bytes.Buffer
+	headersSize uint
 }
 
 func (r *RackResponse) Parse() error {
-	scanner := bufio.NewScanner(r.rackResponse)
-	scanner.Split(bufio.ScanLines)
-	scanner.Scan()
-	status := scanner.Text()
+	r.buf = &bytes.Buffer{}
 
-	code, err := strconv.Atoi(status)
+	// while determining headers size
+	// read(tee) headers into separate
+	// buffer for futher processing
+	reader := bufio.NewReader(io.TeeReader(r.rackResponse, r.buf))
+
+	// end of headers, end of line
+	eol, eoh := false, false
+
+	// at some point reader reader reaches the body
+	r.Body = reader
+
+	// read char by char to correctly land at body start
+	char := make([]byte, 1, 1)
+
+	for {
+		n, err := reader.Read(char)
+
+		if err != nil {
+			return err
+		}
+
+		r.headersSize += uint(n)
+
+		// \n\n marks end of headers
+		eoh = eol && byte('\n') == char[0]
+
+		if eoh {
+			break
+		}
+
+		eol = byte('\n') == char[0]
+	}
+
+	fmt.Println("Read ", r.headersSize, " bytes")
+
+	err := r.ParseHeaders()
 
 	if err != nil {
-		return nil
+		return err
+	}
+
+	return nil
+}
+
+func (r *RackResponse) ParseHeaders() error {
+	var delim = byte('\n')
+
+	// reads headers based on previously determined r.headersSize
+	headers := make([]byte, r.headersSize, r.headersSize)
+	_, err := r.buf.Read(headers)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(headers))
+
+	if err != nil {
+		return err
+	}
+
+	lines := bytes.Split(headers, []byte{delim})
+
+	// first header is a status code
+	code, err := strconv.Atoi(string(lines[0]))
+
+	if err != nil {
+		return err
 	}
 
 	r.Status = code
 
 	r.Headers = make(map[string][]string)
 
-	// r.bodyReaderOffset = scanner.Pos.Offset
-	r.Body = r.rackResponse
+	for _, line := range lines[1:] {
+		hdr := string(line)
+		// fmt.Println(hdr)
 
-	for scanner.Scan() {
-		entry := scanner.Text()
-
-		fmt.Println(entry)
-		// reached body?
-		if entry == "" {
-			return nil
+		if hdr == "" {
+			continue
 		}
 
-		// k: val; val
-		line := strings.SplitN(entry, ": ", 2)
-		vals := strings.Split(line[1], "; ")
-
-		r.Headers[line[0]] = vals
+		kvs := strings.SplitN(hdr, ": ", 2)
+		r.Headers[kvs[0]] = strings.Split(kvs[1], "; ")
 	}
-
-	// if err := scanner.Err(); err != nil {
-	//             fmt.Fprintln(os.Stderr, "There was an error with the scanner in attached container", err)
-	//         }
 
 	return nil
 }
@@ -91,10 +147,16 @@ func TestResponseParse(t *testing.T) {
 		"Set-Cookie":     []string{"UserID=JohnDoe", "Max-Age=3600", "Version=1"},
 	}
 
-	r.Parse()
+	if err := r.Parse(); err != nil {
+		t.Error(err)
+	}
 
 	if !reflect.DeepEqual(r.Headers, result) {
 		t.Errorf("\nExp %s,\nGot %s", result, r.Headers)
+	}
+
+	if r.Body == nil {
+		t.Errorf("r.Body can't be nil")
 	}
 
 	body, err := ioutil.ReadAll(r.Body)
@@ -103,7 +165,7 @@ func TestResponseParse(t *testing.T) {
 		t.Error(err)
 	}
 
-	if string(body) != "hello world" {
+	if reflect.DeepEqual(string(body), "hello world!") {
 		t.Errorf("\nGot %s", string(body))
 	}
 }
