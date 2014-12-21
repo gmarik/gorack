98bbc469c39f6d3177752a9e41e79f4a6e43b580
gmarik <gmarik@gmail.com>
2014-12-21 17:55:39 -0500

    RackHandler test

---
+++ gorack/rack_handler_test.go
@@ -0,0 +1,36 @@
+package gorack
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRackServer(t *testing.T) {
+
+	// package variable
+	gorackRunner = "../ruby/gorack"
+
+	ts := httptest.NewServer(NewRackHandler("../ruby/config_test.ru"))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	got, err := ioutil.ReadAll(res.Body)
+
+	res.Body.Close()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	exp := "hellozzzz"
+
+	if exp != string(got) {
+		t.Errorf("\nGot:%s\nExp:%s", got, exp)
+	}
+}
